43a59e1fd1d7fd3b437878749b5f0ef4fc4d2952
cameloper <cameloper@cameloper.com>
2024-05-06 04:17:01 +0200

    Created a new py file to load one or more html files and parse them.

---
+++ load_and_parse.py
@@ -0,0 +1,38 @@
+import kvbw_html_parser as kvbw
+import sys
+import glob
+
+def parse_dir(dir):
+    paths = glob.glob("{}/*.html".format(dir))
+    print("Found {} html files in {}.".format(len(paths), dir))
+    return parse(paths)
+
+def parse(paths):
+    arzts = list()
+    for path in paths:
+        try:
+            with open(path, 'r') as f:
+                p_arzts = kvbw.parse_all_resultrows(f)
+                arzts = arzts + p_arzts
+        except:
+            sys.exit("Could not parse file: {}.".format(path))
+
+    print("Parsed total number of {} arzts.".format(len(arzts)))
+    return arzts
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit('Please enter a command! Valid commands are: parse-files and parse-directory.')
+
+    command = sys.argv[1]
+    if command == 'parse-files':
+        if len(sys.argv) < 3:
+            sys.exit('Command parse-files requires at least one file path.')
+        return parse(sys.argv[2:])
+    elif command == 'parse-directory':
+        if len(sys.argv) != 3:
+            sys.exit('Command parse-directory requires exactly one directory path.')
+        return parse_dir(sys.argv[2])
+
+if __name__ == "__main__":
+    main()
